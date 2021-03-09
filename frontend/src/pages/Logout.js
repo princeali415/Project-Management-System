3922f6c5c8e7eea9d005ad61855eb2eba508661e
Hussain Ali <ali.415code@gmail.com>
2021-03-08 17:56:49 -0800

    Completed Logout

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import '../App.css'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    },
+  }));
+  
 
 export default function Logout() {
-
+    
+    const classes = useStyles();
     const history = useHistory();
 
     const goHome = () => {
@@ -11,8 +29,12 @@ export default function Logout() {
 
     return (
         <div>
-            <h1>Succesfully Logged Out!</h1>
-            <button onClick={goHome}>Go To Login</button>
+        <div className={classes.root} id="logout">
+            <Typography align='center'>Great work today, enjoy the rest of your day!</Typography>
+            <div>
+            <Button variant="contained" color="secondary" onClick={goHome} size='large' id='login'>Login</Button>
+            </div>
+        </div>
         </div>
     )
 }

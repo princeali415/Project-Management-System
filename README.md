eff36699909b709637c3ee2dd47c5e9536468c4b
Hussain Ali <ali.415code@gmail.com>
2021-03-27 18:40:57 -0700

    Updated To Do List

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-Management-System
 A personal project management system for software developers to track multiple projects they are working on.
+
+# To Do by 3/31/21
+1. fix backend so current user info shows projects list (user needs to have a many to one relationship with projects)
+2. add project functionality
--- frontend/src/components/TicketTable/AddTicketForm.js
+++ frontend/src/components/TicketTable/AddTicketForm.js
@@ -4,34 +4,34 @@ export default function AddTicketForm() {
 
     const userInfo = JSON.parse(localStorage.getItem('userinfo'))
 
-    const initialFormValues = {
-        problemname: "Test add",
-        problemdescription: "Test add description",
-        problemtype: {
-            problemtypeid: 2,
-            problemtype: "Task"
-        },
-        project: {
-            projectid: 2,
-            projectname: "",
-            projectdescription: ""
-        },
-        user: {
-            userid: userInfo.userid,
-            username: userInfo.username,
-            userrole: {
-                roletypeid: userInfo.userrole.roletypeid,
-                roletype: userInfo.userrole.roletype
-            },
-            email: userInfo.email
-        },
-        status: {
-            statustypeid: 1,
-            status: "Open Ticket"
-        }
-      }
+    // const initialFormValues = {
+    //     problemname: "Test add",
+    //     problemdescription: "Test add description",
+    //     problemtype: {
+    //         problemtypeid: 2,
+    //         problemtype: "Task"
+    //     },
+    //     project: {
+    //         projectid: 2,
+    //         projectname: "",
+    //         projectdescription: ""
+    //     },
+    //     user: {
+    //         userid: userInfo.userid,
+    //         username: userInfo.username,
+    //         userrole: {
+    //             roletypeid: userInfo.userrole.roletypeid,
+    //             roletype: userInfo.userrole.roletype
+    //         },
+    //         email: userInfo.email
+    //     },
+    //     status: {
+    //         statustypeid: 1,
+    //         status: "Open Ticket"
+    //     }
+    //   }
 
-
+    
 
 
 

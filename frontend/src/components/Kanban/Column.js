fc9f454b5fe865746a7e2f82e50a9f3023173c05
Hussain Ali <ali.415code@gmail.com>
2021-04-05 21:58:45 -0700

    added key to column

--- frontend/src/components/Kanban/Column.js
+++ frontend/src/components/Kanban/Column.js
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react'
 import './kanban.css'
 
 
-export default function Column({item}) {
+export default function Column({item, key}) {
 
     return (
-        <div className={item.cName}>
+        <div className={item.cName} key={key}>
             {item.title}
         </div>
     )
